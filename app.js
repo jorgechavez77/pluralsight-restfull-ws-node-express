87d5f114309c290f1df4f294393dc65e264e97f8
Jorge Chavez <jachavez@cngholdings.com>
2017-07-13 15:06:04 -0400

    Getting single item

--- app.js
+++ app.js
@@ -30,6 +30,25 @@ bookRouter.route('/books')
         })
     });
 
+bookRouter.route('/books/:id')
+    .get((req, res) => {
+        console.log(JSON.stringify(req.params));
+        var _id = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(_id)) {
+            var response = {message: 'Invalid param _id = ' + _id, error: '400' };
+            res.status(400).json(response);
+        }
+
+        Book.findById(_id, (err, book) => {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(book);
+            }
+        })
+    });
+
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
@@ -42,3 +61,4 @@ app.listen(port, () => {
 
 // example
 // http://localhost:8000/api/books?genre=Historical%20Fiction&author=Lev%20Nikolavich%20Tolstoy
+// http://localhost:8000/api/books/5967b28e9a51582b9b6bf0b1
